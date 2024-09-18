50832a51957e6a94945cd08552b480396ca72127
Aaron Magat <115564271+Arnought@users.noreply.github.com>
2024-09-18 16:21:59 +0800

    Attached NavBar to Reservation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Reservation from './SBOX/SBOX_Magat/Reservation';
+import About from './SBOX/SBOX_Isidro/SBOX_About';
+import Home from './Core/Home';
 
 function App() {
+  document.title = "Home";
   return (
-    <div>
-      <Reservation />
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/reservation' element={<Reservation />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
