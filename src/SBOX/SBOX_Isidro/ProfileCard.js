b1bab00e6a44dadd60783c1de9fa456fa96fd04b
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-11-28 11:44:09 +0800

    Fixed about page

--- src/SBOX/SBOX_Isidro/ProfileCard.js
+++ src/SBOX/SBOX_Isidro/ProfileCard.js
@@ -4,7 +4,6 @@ import './about.css';
 export default function ProfileCard(props){
     return (
         <div id="container">
-            <img src={props.img} alt={props.alt} id="pfp"></img>
             <p id="name"><strong>{props.name}</strong></p>
         </div>
     );
